91706840a7c163bac73f2d6c2651888e71c91bf5
aminkhatibi <aminkhatibi@icloud.com>
2022-05-14 12:43:13 +0200

    preferenties versturen naar db

--- screens/FavouriteScreen.js
+++ screens/FavouriteScreen.js
@@ -1,4 +1,4 @@
-import { Text, ScrollView, StyleSheet, Button, TextInput} from 'react-native';
+import { Text, View ,ScrollView, StyleSheet, Button, TextInput, Alert} from 'react-native';
 import React, { useState, useEffect } from 'react';
 import {Picker} from '@react-native-picker/picker';
 import authAxios from '../apis/AuthApi';
@@ -6,11 +6,12 @@ import authAxios from '../apis/AuthApi';
 export default function FavouriteScreen() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [idStudent, setIdStudent] = useState();
+  const [idStudent, setIdStudent] = useState(15);
   const [firstChoice, setFirstChoice] = useState();
   const [secondChoice, setSecondChoice] = useState();
   const [thirdChoice, setThirdChoice] = useState();
-  const [submitted, setSubmitted] = useState();
+  const [submitted, setSubmitted] = useState(false);
+  const [showBox, setShowBox] = useState(true);
 
   useEffect(() => {
     authAxios.get("/thesis/all")
@@ -26,6 +27,79 @@ const renderSubjectList = () => {
     return <Picker.Item label={thesis.name} value={thesis.id} key={thesis.id} />
   })
 }
+const preferencesData = {
+  "idStudent": 15,
+  "firstChoice": firstChoice,
+  "secondChoice": secondChoice,
+  "thirdChoice": thirdChoice,
+  "submitted": submitted
+}
+const onSubmitFormHandler = async () => {
+  if (!firstChoice || !secondChoice || !thirdChoice) {
+    alert("Please complete your selection.");
+    return;
+  }
+  if (firstChoice==secondChoice || secondChoice==thirdChoice || thirdChoice==firstChoice){
+    alert("Please select three different subjects, selecting the same subject more than once is not allowed.");
+    return;
+  }
+  return Alert.alert(
+    "Alert",
+    "Are you sure you want to submit your choices?",
+    [
+      // The "Yes" button
+      {
+        text: "Yes",
+        onPress: () => {
+          setSubmitted(true);
+          setShowBox(false);
+          
+          try {
+            authAxios.post("/preferences/add", preferencesData).then(response=>{
+              console.log(response)
+              console.log(preferencesData)
+            })
+          } catch (error) {
+            console.log(error);
+            setLoading(false);
+          }
+        },
+      },
+      // The "No" button
+      // Does nothing but dismiss the dialog when tapped
+      {
+        text: "No",
+      },
+    ]
+  );
+  setLoading(true);
+  
+  
+};
+const onSaveFormHandler = async () => {
+  if (!firstChoice || !secondChoice || !thirdChoice) {
+    alert("Please complete your selection.");
+    return;
+  }
+  if (firstChoice==secondChoice || secondChoice==thirdChoice || thirdChoice==firstChoice){
+    alert("Please select three different subjects, selecting the same subject more than once is not allowed.");
+    return;
+  }
+  setLoading(true);
+  
+  try {
+    authAxios.post("/preferences/add", preferencesData).then(response=>{
+      console.log(response)
+      console.log(preferencesData)
+    })
+  } catch (error) {
+    console.log(error);
+    setLoading(false);
+  }
+  alert("Your selection has been saved, if you are sure of this selection you can submit it.")
+  setLoading(false);
+};
+
   return (
     <ScrollView>
       <Text style={styles.title}>
@@ -39,6 +113,7 @@ const renderSubjectList = () => {
       selectedValue={firstChoice}
       onValueChange={(itemValue, itemIndex) =>
       setFirstChoice(itemValue)}
+      itemStyle={{height:130}}
   >
       <Picker.Item label="Please select your first option..." value="" />
 			{renderSubjectList()}
@@ -49,6 +124,7 @@ const renderSubjectList = () => {
         Choice number 2:
       </Text>
       <Picker 
+      itemStyle={{height:130}}
       selectedValue={secondChoice}
       onValueChange={(itemValue, itemIndex) =>
       setSecondChoice(itemValue)}
@@ -61,6 +137,7 @@ const renderSubjectList = () => {
         Choice number 3:
       </Text>
       <Picker 
+      itemStyle={{height:130}}
       selectedValue={thirdChoice}
       onValueChange={(itemValue, itemIndex) =>
       setThirdChoice(itemValue)}
@@ -69,6 +146,21 @@ const renderSubjectList = () => {
 			{renderSubjectList()}
 
 		</Picker>
+    
+        <View style={styles.fixToText}>
+        <Button
+            title="Save"
+            onPress={onSaveFormHandler}
+            style={styles.saveButton}
+          
+          />
+          <Button
+            title="Submit"
+            onPress={onSubmitFormHandler}
+            style={styles.submitButton}
+            
+          />
+        </View>
     </ScrollView>
     
   );
@@ -88,6 +180,7 @@ const styles = StyleSheet.create({
     color: 'steelblue',
     borderBottomColor: 'steelblue',
     borderBottomWidth: 1,
+    marginHorizontal: 5
   },
   subtitle:{
     fontSize:15,
@@ -99,5 +192,16 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     height: 1,
     width: '80%',
+  },
+  saveButton:{
+
+  },
+  submitButton:{
+
+  },
+  fixToText: {
+    marginTop:5,
+    marginHorizontal:10,
+   
   },
 });
